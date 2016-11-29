6b4024b84ddaa89680c307f63ac552253e7ead8a
Oduye Oluwayemisi <oduye.oluwayemisi@andela.com>
2016-11-29 12:52:18 +0100

    Create a save route to make POST request to HubSpot Contact API #19

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
+const hubspotKey = process.env.HUBSPOT_KEY;
+
+app.use(bodyParser.json());
+app.enable('trust proxy');
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, Content-Type, Accept");
+  return next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Welcome to HubSpot Service');
+});
+
+// Handle POST request to '/save'
+app.post('/save', function(req, res, next) {
+  request({
+    url: 'https://api.hubapi.com/contacts/v1/contact/?hapikey=' + hubspotKey,
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    json: req.body,
+  }, function(error, response, body) {
+    res.json({
+      'statusCode' : response.statusCode,
+      'body' : response.body
+    });
+  })
+
+});
+
+app.listen(3000).on('listening', function() { console.log ('server listening') });
